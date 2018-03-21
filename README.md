f9ad59aa7594157b8f3ddd95e934112904b090f4
witiger <info@witiger.ru>
2018-03-21 19:11:20 +0300

    Error correction & readme edition

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # hello-world
 First repository
 
+21.03.2018 index.php created on localhost
+21.03.2018 README edit on localhost
+21.03.2018 README edit on GitHub
 
-1. README edit on GitHub
-2. README edit on localhost 3
-3. README edit on localhost 4 
-4. README edit on localhost 5 
+
 
 
--- index.php
+++ index.php
@@ -5,15 +5,14 @@
  <body>
  <?php 
  
- echo '<div align=center><b>PHP || heroku & GitHub Test</b></p>'; 
+ echo '<div align=center>'; 
  echo '<p><b>PHP || heroku & GitHub Test</b></p>'; 
  echo '<p>Тестовая ветка</p>'; 
  echo '<p>https://github.com/witiger/hello-world/tree/site-edit</p>'; 
  echo '<p>Боевая ветка ветка</p>'; 
  echo '<p>https://github.com/witiger/hello-world/tree/master</p>';
  echo '</div>';  
- 
- 
+  
  ?> 
  </body>
 </html>
