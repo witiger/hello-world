e3748a1ec88d8f9d6858130f21f0a494abd3effe
witiger <info@witiger.ru>
2018-03-22 16:30:24 +0300

    css fixing

--- tech.php
+++ tech.php
@@ -59,26 +59,11 @@
 					<hr>	
 
 					<h3>Параметры сервера</h3>
-					<table class="table table-striped">
+					<table class="table table-striped" cellpadding="10" border="1" width="500">
 						<?php phpinfo(); ?>
 						<tbody>
 							<tr>
-								<th scope="row">1</th>
-								<td>Mark</td>
-								<td>Otto</td>
-								<td>@mdo</td>
-							</tr>
-							<tr>
-								<th scope="row">2</th>
-								<td>Jacob</td>
-								<td>Thornton</td>
-								<td>@fat</td>
-							</tr>
-							<tr>
-								<th scope="row">3</th>
-								<td>Larry</td>
-								<td>the Bird</td>
-								<td>@twitter</td>
+								<td wigth="500">Mark</td>
 							</tr>
 						</tbody>
 					</table>					
